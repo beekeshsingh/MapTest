5c755bc752553b473b93439335132843274f7391
Beekesh Singh <er.beekeshsingh@gmail.com>
2017-05-26 12:42:46 +0530

    AddProject

--- app/src/main/java/com/example/bsingh/DetailTabActivity.java
+++ app/src/main/java/com/example/bsingh/DetailTabActivity.java
@@ -7,15 +7,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.bsingh.maptest.R;
 
 public class DetailTabActivity extends Fragment {
 
+    TextView textView_lat,textView_lng;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_detail_tab,null);
+        textView_lat = (TextView)view.findViewById(R.id.textView_lat);
+        textView_lng = (TextView)view.findViewById(R.id.textView_lng);
         return view;
     }
 }
